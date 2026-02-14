e7d3e8373879304f4c6b937538899023cb1a06f3
sdjdll <sdjini4491@163.com>
2026-02-14 16:13:55 +0800

    Don't in def pac

--- Windows/src/Main/Server.java
+++ Windows/src/Main/Server.java
@@ -1,6 +1,11 @@
+package Main;
+
+import CLIPBOARD.Clipboard;
 import NET.Net;
 
 public class Server {
+    public static Clipboard MainClipboard = new Clipboard();
+
     public static void main(String[] args) {
         Net net = new Net();
     }
