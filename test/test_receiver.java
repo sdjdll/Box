38d0e677fdc0b922a2dcf72ba7ad85c1e26666f0
sdjini <sdjini4491@163.com>
2026-07-17 13:16:14 +0800

    测试用例：接收

--- test/test_receiver.java
+++ test/test_receiver.java
@@ -5,7 +5,7 @@ void main() throws IOException {
 	Client c = new Client("test_receiver", InetAddress.getByName("localhost"), 11011);
 	new Thread(()->{
 		try {
-			System.out.println(new String(c.receive(), StandardCharsets.UTF_8));
+			while(true) System.out.println(new String(c.receive(), StandardCharsets.UTF_8));
 		} catch (EOSException _) {}
 	}).start();
 }
